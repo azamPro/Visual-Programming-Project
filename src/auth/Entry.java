80514a8d0ce2f97e86415f1a9af4cc4c75c2e73a
Mohammed-Alzain <mohammeddalzain@gmail.com>
2025-03-27 07:08:11 +0300

    Added login as admin or user

--- src/auth/Entry.java
+++ src/auth/Entry.java
@@ -9,17 +9,38 @@ public class Entry extends JFrame {
 
     public Entry() {
         setTitle("Entry");
-        setSize(450, 550);
+        setSize(450, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         // def
-        Entry = new JLabel("Loggin in as?");
+        Entry = new JLabel("Signinig in as?");
         Admin = new JButton("Admin");
         User = new JButton("User");
 
+        Entry.setFont(new Font("Segoe UI", Font.BOLD, 32));
+        Entry.setBounds(120, 40, 200, 40);
+
+        User.setFont(new Font("Segoe UI", Font.BOLD, 16));
+        User.setBounds(50, 200, 150, 45);
+        User.setBorder(null);
+        User.setBackground(Color.gray);
+        User.setForeground(Color.white);
+
+        Admin.setFont(new Font("Segoe UI", Font.BOLD, 16));
+        Admin.setBounds(250, 200, 150, 45);
+        Admin.setBorder(null);
+        Admin.setBackground(Color.GRAY);
+        Admin.setForeground(Color.white);
+
         JPanel p = (JPanel) this.getContentPane();
         p.setLayout(null);
+        p.setBackground(Color.white);
+        p.add(Entry);
+        p.add(Admin);
+        p.add(User);
+
+        this.setVisible(true);
 
     }
 }
